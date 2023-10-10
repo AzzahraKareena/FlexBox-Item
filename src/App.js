395cb25e2b5e2625fb54bdc215d03fa8daad8b07
Azzahra Kareena <azzahrakareena30@student.uns.ac.id>
2023-10-10 21:31:46 +0700

    Menambahkan folder src/Components dan src/Style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // File CSS utama
+import AlignItems from './Components/AlignItems'; 
+import AlignSelf from './Components/AlignSelf'; 
+import AlignContent from './Components/AlignContent';
+import FlexWrap from './Components/FlexWrap';
+import FlexBasis from './Components/FlexBasis-Grow-Shrink';
+import Gap from './Components/GapRowandColumn';
+import Layout from './Components/AbsoluteRelativeLayout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contoh Penerapan Flexbox Align Items</h1>
+      <AlignItems />
+      <h1>Contoh Penerapan Flexbox Align Self</h1>
+      <AlignSelf />
+      <h1>Contoh Penerapan Flexbox Align Content</h1>
+      <AlignContent />
+      <h1>Contoh Penerapan Flexbox Flex Wrap</h1>
+      <FlexWrap />
+      <h1>Contoh Penerapan Flexbox Flex Basis, Grow, dan Shrink</h1>
+      <FlexBasis/>
+      <h1>Contoh Penerapan Flexbox Row Gap, Column Gap, dan Gap</h1>
+      <Gap/>
+      <h1>Contoh Penerapan Flexbox Flex Absolute dan Relative Layout</h1>
+      <Layout/>
+      {/* Komponen-komponen lain */}
     </div>
   );
 }
